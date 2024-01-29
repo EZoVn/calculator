ab1a7b191d85d524d43702e28d5951ffad667f36
EzoVn <vincent.galpin@gmail.com>
2024-01-29 15:09:51 +0100

    theme ok, responsive a suivre

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,19 @@ module.exports = {
     extend: {
       colors: {
         'bg-body': "var(--bg-body)",
-        'background-color-input': "var(--background-input-color)",
+        'background-input-color': "var(--background-input-color)",
         'color': "var(--color)",
         'number-color': "var(--number-color)",
-        'touch-color': "var(--touch-color)",
+        'touch': "var(--touch)",
+        'bg-calculator': "var(--bg-calculator)",
+        'number-color-delete': "var(--number-color-delete)",
+        'touch-equals': "var(--touch-equals)",
+        'touch-shadow': "var(--touch-shadow)",
+        'touch-shadow-delete': "var(--touch-shadow-delete)",
+        'touch-shadow-equals': "var(--touch-shadow-equals)",
+        'touch-hover': "var(--touch-hover)",
+        'touch-hover-delete': "var(--touch-hover-delete)",
+        'touch-hover-equals': "var(--touch-hover-equals)",
       }
     },
   },
