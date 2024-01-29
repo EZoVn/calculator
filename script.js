54f9390a60dbdd836f2c5fda02624560fac345f7
EzoVn <vincent.galpin@gmail.com>
2024-01-29 13:29:59 +0100

    challenge calculate frontend_Mentor

---
+++ script.js
@@ -0,0 +1,46 @@
+const output = document.getElementById('output');
+const equals = document.getElementById('equals');
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach((button) => {
+  button.addEventListener('click', handleButtonClick);
+});
+
+function handleButtonClick(event) {
+  const btnValue = event.target.innerText;
+  switch (btnValue) {
+    case 'Reset':
+      output.innerText = '';
+      break;
+    case '.':
+      if (output.innerText.includes('.')) return;
+      output.innerText += btnValue;
+      break;
+    case 'DEL':
+      output.innerText = output.innerText.slice(0, -1);
+      break;
+    case '=':
+      output.innerText = math.evaluate(output.innerText);
+      break;
+    default:
+      output.innerText += btnValue;
+  }
+}
+
+const themeSwitch = document.querySelectorAll('input[name="theme"]');
+const calculatorSection = document.getElementById('calculator');
+
+
+const toggle = document.getElementById('toggle');
+const toggleBall = document.getElementById('toggle-ball');
+
+let toggleState = 0;
+
+toggle.addEventListener('click', () => {
+  toggleState = (toggleState + 1) % 3;
+  console.log(toggleState);
+  toggleBall.style.left = `${toggleState * 1.25}rem`;
+  themeSwitch[toggleState].checked = true;
+  calculatorSection.classList = `theme${toggleState + 1}`;
+});
